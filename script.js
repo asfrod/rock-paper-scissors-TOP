6ea760a0369e187ec23ecbdc74fdb6e36e353750
asfrod <andresperez192@gmail.com>
2022-06-24 12:25:38 -0500

    Se creó un juego de piedra, papel y tijeras contra el computador completamente funcional

---
+++ script.js
@@ -0,0 +1,91 @@
+// Vamos a hacer un juego de piedra, papel y tijeras:
+
+
+// Selecciona de manera aleatoria una de las tres opciones
+function computerPlay() {
+    let option = ["rock", "paper", "scissors"];
+    
+    const random = Math.floor(Math.random() * option.length);
+    const select = option[random] ;
+    //console.log(select)
+    return select;
+}
+
+//computerPlay()
+
+// Corre el juego utilizando como entradas la elección del jugador y la elección aleatoria de la anterior función
+function rps(playerSelection, computerSelection) {
+
+    //console.log(playerSelection);
+    //console.log(computerSelection);
+
+    if (playerSelection == "rock" && computerSelection == "scissors") {
+        console.log("Ganaste");
+        const resultado = 1;
+        return resultado;
+    }
+
+    else if (playerSelection == "paper" && computerSelection == "rock") {
+        console.log("Ganaste");
+        const resultado = 1;
+        return resultado;
+    }
+
+    else if (playerSelection == "scissors" && computerSelection == "paper") {
+        console.log("Ganaste");
+        const resultado = 1;
+        return resultado;
+    }
+
+    else if (playerSelection == computerSelection) {
+        console.log("Empate, sigue jugando");
+        const resultado = 0;
+        return resultado;
+    }
+
+    else {
+        console.log("Perdiste hasta los calzoncillos")
+        const resultado = -1;
+        return resultado;
+    }
+    
+}
+
+//const computer = computerPlay();
+//const ply = prompt("¿Cúal es tu elección?: rock, paper, scissors").toLocaleLowerCase()
+//const player = "rocK";
+//const ply = player.toLocaleLowerCase();
+
+//rps(ply, computer)
+//console.log(rps(ply, computer))
+
+
+function game() {
+    let b = 0;      //inicializo la variable b para almacenar la suma de los resultados de cada juego
+    alert("Recuerda abrir la consola")
+    for (let i = 0; i<5; i++) {
+        const computer = computerPlay();    //cada vez que inicia un ciclo se genera una elección aleatoria para la máquina
+        const ply = prompt("¿Cúal es tu elección?: rock, paper, scissors").toLocaleLowerCase()
+        //const player = "rocK";
+        //const ply = player.toLocaleLowerCase();
+    
+        const a = rps(ply, computer);
+        //console.log(a);
+        b = b + a;
+    }
+    //console.log(b)
+
+    console.log("------------------------------");    
+
+    if (b>0) {
+        console.log("Ganaste el juego");
+    }
+    else if (b<0) {
+        console.log("Perdiste el juego");
+    }
+    else {
+        console.log("Que buen empate");
+    }
+}
+
+game()
