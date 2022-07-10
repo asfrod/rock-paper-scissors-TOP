d4ec5d5e13bd5f5faccbc7407135d4a99eb7fc00
asfrod <andresperez192@gmail.com>
2022-07-10 12:54:56 -0500

    Se muestran los resultados añadiendo un elemento html con js

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ function computerPlay() {
     
     const random = Math.floor(Math.random() * option.length);
     const select = option[random] ;
-    //console.log(select)
     return select;
 }
 
@@ -17,65 +16,56 @@ function computerPlay() {
 // Corre el juego utilizando como entradas la elección del jugador y la elección aleatoria de la anterior función
 function rps(playerSelection, computerSelection) {
 
-    //console.log(playerSelection);
-    //console.log(computerSelection);
-
     if (playerSelection == "rock" && computerSelection == "scissors") {
-        console.log("Ganaste");
-        const resultado = 1;
-        return resultado;
+        
+
+        //Creando un nuevo elemento div dentro de body para mostrar el resultado
+        const divResult = document.querySelector('.results');
+        divResult.textContent = 'Has Ganado!!!!!';
+
+        return a = 1;;
     }
 
     else if (playerSelection == "paper" && computerSelection == "rock") {
-        console.log("Ganaste");
-        const resultado = 1;
-        return resultado;
+        //Creando un nuevo elemento div dentro de body para mostrar el resultado
+        const divResult = document.querySelector('.results');
+        divResult.textContent = 'Has Ganado!!!!!';
+        return a = 1;
     }
 
     else if (playerSelection == "scissors" && computerSelection == "paper") {
-        console.log("Ganaste");
-        const resultado = 1;
-        return resultado;
+        //Creando un nuevo elemento div dentro de body para mostrar el resultado
+        const divResult = document.querySelector('.results');
+        divResult.textContent = 'Has Ganado!!!!!';       
+        return a = 1;
     }
 
     else if (playerSelection == computerSelection) {
-        console.log("Empate, sigue jugando");
-        const resultado = 0;
-        return resultado;
+        //Creando un nuevo elemento div dentro de body para mostrar el resultado
+        const divResult = document.querySelector('.results');
+        divResult.textContent = 'Empate, que locura!!!!!';      
+        return a = 0;
     }
 
     else {
-        console.log("Perdiste hasta los calzoncillos")
-        const resultado = -1;
-        return resultado;
+        //Creando un nuevo elemento div dentro de body para mostrar el resultado
+        const divResult = document.querySelector('.results');
+        divResult.textContent = 'Perdiste, que loca';       
+        return a = -1;
     }
 }
-
-//rps("paper", computerPlay())
-
-//const computer = computerPlay();
-//const ply = prompt("¿Cúal es tu elección?: rock, paper, scissors").toLocaleLowerCase()
-//const player = "rocK";
-//const ply = player.toLocaleLowerCase();
-
-//rps(ply, computer)
-//console.log(rps(ply, computer))
 
 
 function game() {
     let b = 0;      //inicializo la variable b para almacenar la suma de los resultados de cada juego
-    alert("Recuerda abrir la consola")
+    //alert("Recuerda abrir la consola")
     for (let i = 0; i<5; i++) {
         const computer = computerPlay();    //cada vez que inicia un ciclo se genera una elección aleatoria para la máquina
         const ply = prompt("¿Cúal es tu elección?: rock, paper, scissors").toLocaleLowerCase()
-        //const player = "rocK";
-        //const ply = player.toLocaleLowerCase();
-    
+        
         const a = rps(ply, computer);
-        //console.log(a);
         b = b + a;
     }
-    //console.log(b)
 
     console.log("------------------------------");    
 
@@ -95,6 +85,9 @@ function game() {
 
 
 
+
+//Se llama cada botón y se le asigna el argumento a ingresar en la función
+//para jugar
 
 
 const rck = document.querySelector("#roca");
